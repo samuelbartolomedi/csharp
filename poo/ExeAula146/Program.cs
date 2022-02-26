c0f47a8b6db087f0f317d8487ec7a3d418862091
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-02-25 21:29:13 -0300

    exercicios de lógica e poo

---
+++ poo/ExeAula146/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExeAula146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
