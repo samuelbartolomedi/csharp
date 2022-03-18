3123e4eb8d5ac9254472a25da75dc212e4938cf6
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-03-18 17:27:16 -0300

    exercicios de lógica e poo

---
+++ poo/ExeAula242/ExeAula242.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ExeAula242.Entities;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ExeAula242
+{
+    class ExeAula242
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Salary limit? ");
+            double salaryLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] 
+                    }
+                }
+            }
+
+        }
+    }
+}
