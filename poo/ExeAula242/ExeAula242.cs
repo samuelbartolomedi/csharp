b8ac246ace20ed0dc34c8cf4cb717b5dd814b05e
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-03-21 11:11:20 -0300

    exercicios de lógica e poo

--- poo/ExeAula242/ExeAula242.cs
+++ poo/ExeAula242/ExeAula242.cs
@@ -3,6 +3,7 @@ using System.IO;
 using ExeAula242.Entities;
 using System.Globalization;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ExeAula242
 {
@@ -15,7 +16,7 @@ namespace ExeAula242
             Console.Write("Salary limit? ");
             double salaryLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            List<Employee> list = new List<Employee>();
+            List<Employee> employeeList = new List<Employee>();
 
             try
             {
@@ -23,11 +24,34 @@ namespace ExeAula242
                 {
                     while (!sr.EndOfStream)
                     {
-                        string[] 
+                        string[] employeeData = sr.ReadLine().Split(",");
+                        string name = employeeData[0];
+                        string email = employeeData[1];
+                        double salary = double.Parse(employeeData[2], CultureInfo.InvariantCulture);
+                        employeeList.Add(new Employee(name, email, salary));
                     }
+                    //cria var emails, filtra datasource pelo limite informado, ordena por email e tras na tela os email da restrição
+                    var emails = employeeList.Where(obj => obj.Salary > salaryLimit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+                    
+                    //cria var sum, filtra os nomes começados em M e faz a soma dos salários desse filtro
+                    var sum = employeeList.Where(obj => obj.Name[0] == 'M').Sum(obj => obj.Salary);
+
+                    Console.WriteLine($"Email of people whose salary is more than {salaryLimit.ToString("F2", CultureInfo.InvariantCulture)}: ");
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+
+
+                    Console.WriteLine($"Sum of salary of people whose name starts whit 'M': {sum.ToString("F2", CultureInfo.InvariantCulture)}");
                 }
+
             }
 
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
