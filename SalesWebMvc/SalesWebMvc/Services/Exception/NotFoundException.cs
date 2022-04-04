96546f69463ca02c6942756cb544a3427fa204d8
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-04-04 17:57:04 -0300

    projeto SalesWeb com MVC e Entity Framework

---
+++ SalesWebMvc/SalesWebMvc/Services/Exception/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
