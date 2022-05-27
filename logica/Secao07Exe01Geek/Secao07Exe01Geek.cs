dd9de9d4d51fb0d23080ce0800f768e50fa58280
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-05-27 17:46:50 -0300

    exes Geek University

---
+++ logica/Secao07Exe01Geek/Secao07Exe01Geek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Secao07Exe01Geek
+{
+    class Secao07Exe01Geek
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            double maior = 0;
+            while (valor != 0)
+            {
+                if (valor > maior)
+                {
+                    maior = valor;
+                }
+
+                Console.WriteLine("Informe um valor: ");
+                valor = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"O maior valor foi: {maior.ToString("F2")}");
+        }
+    }
+}
---
+++ logica/Secao07Exe02Geek/Secao07Exe02Geek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Secao07Exe02Geek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+
+                if (i % 10 == 0)
+                {
+                    Console.Write($"{i} é Multiplo de 10\n");
+                }
+            }
+        }
+    }
+}
