a5e2632f6702a40bc4dc0615ffd8252c5f4b7d24
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-05-27 10:58:53 -0300

    exes Geek University

---
+++ logica/Secao07Exe01Geek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secao07Exe01Geek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
