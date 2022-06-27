f74eec6bbb1ee3f82e1a560ae41ed8239528802b
placebofx94 <samuelbartolomedi1994@gmail.com>
2022-06-26 22:45:50 -0300

    desafio imposto devsuperior

--- DevSuperior/DesafioImposto/DesafioImposto.cs
+++ DevSuperior/DesafioImposto/DesafioImposto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Recognizers.Text;
+using System;
+using System.Globalization;
 
 namespace DesafioImposto
 {
@@ -7,15 +9,15 @@ namespace DesafioImposto
         static void Main(string[] args)
         {
             Console.WriteLine("Informe a renda anual com salário: ");
-            double rendaAnualSalario = double.Parse(Console.ReadLine());
+            double rendaAnualSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Informe a renda anual com prestação de serviço: ");
-            double rendaAnualServico = double.Parse(Console.ReadLine());
+            double rendaAnualServico = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Informe a renda anual com ganho de capital: ");
-            double rendaAnualCapital = double.Parse(Console.ReadLine());
+            double rendaAnualCapital = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Informe os gastos médicos: ");
-            double gastosMedicos = double.Parse(Console.ReadLine());
+            double gastosMedicos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Informe os gastos educacionais: ");
-            double gastosEducacionais = double.Parse(Console.ReadLine());
+            double gastosEducacionais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             double impostoSalario = 0;
             double impostoServico = 0;
@@ -29,7 +31,7 @@ namespace DesafioImposto
             {
                 impostoSalario = rendaAnualSalario * 0.0;
             }
-            
+
             if (rendaAnualSalario >= 36000 && rendaAnualSalario <= 60000)
             {
                 impostoSalario = rendaAnualSalario * 0.10;
@@ -42,40 +44,68 @@ namespace DesafioImposto
 
             if (rendaAnualServico > 0.0)
             {
-                impostoServico = rendaAnualServico * 0.20;
+                impostoServico = rendaAnualServico * 0.15;
+            }
+            else
+            {
+                impostoServico = rendaAnualServico * 0.0;
             }
 
             if (rendaAnualCapital > 0.0)
             {
                 impostoCapital = rendaAnualCapital * 0.20;
             }
+            else
+            {
+                impostoCapital = rendaAnualCapital * 0.0;
+            }
 
-            
             brutoImpostos = impostoCapital + impostoSalario + impostoServico;
-
-            totalGastos = gastosMedicos + gastosEducacionais;
 
             maxDedutivel = brutoImpostos * 0.30;
 
-            liquidoImpostos = brutoImpostos - maxDedutivel;
+            totalGastos = gastosMedicos + gastosEducacionais;
 
-            
+            if (totalGastos > maxDedutivel)
+            {
+                liquidoImpostos = brutoImpostos - maxDedutivel;
 
-            Console.WriteLine("RELATÓRIO DE IMPOSTO DE RENDA\n" +
-                "\n" +
-                "CONSOLIDADO DE RENDA:\n" +
-                $"Imposto sobre salario: {impostoSalario}\n" +
-                $"Imposto sobre serviços: {impostoServico}\n" +
-                $"Imposto sobre ganho de capital: {impostoCapital}\n"+
-                "\n" +
-                "DEDUÇÕES:\n" +
-                $"Máximo dedutível: {maxDedutivel}\n" +
-                $"Gastos dedutíveis: {totalGastos}\n" +
-                "\n" +
-                "RESUMO:\n" +
-                $"Imposto bruto total: {brutoImpostos}\n" +
-                $"Abatimento: {maxDedutivel}\n" +
-                $"Imposto devido: {liquidoImpostos}");
+                Console.WriteLine("RELATÓRIO DE IMPOSTO DE RENDA\n" +
+                    "\n" +
+                    "CONSOLIDADO DE RENDA:\n" +
+                    $"Imposto sobre salario: {impostoSalario.ToString("F2")}\n" +
+                    $"Imposto sobre serviços: {impostoServico.ToString("F2")}\n" +
+                    $"Imposto sobre ganho de capital: {impostoCapital.ToString("F2")}\n" +
+                    "\n" +
+                    "DEDUÇÕES:\n" +
+                    $"Máximo dedutível: {maxDedutivel.ToString("F2")}\n" +
+                    $"Gastos dedutíveis: {totalGastos.ToString("F2")}\n" +
+                    "\n" +
+                    "RESUMO:\n" +
+                    $"Imposto bruto total: {brutoImpostos.ToString("F2")}\n" +
+                    $"Abatimento: {maxDedutivel.ToString("F2")}\n" +
+                    $"Imposto devido: {liquidoImpostos.ToString("F2")}");
+            }
+            else
+            {
+                liquidoImpostos = brutoImpostos - totalGastos;
+
+                Console.WriteLine("RELATÓRIO DE IMPOSTO DE RENDA\n" +
+                    "\n" +
+                    "CONSOLIDADO DE RENDA:\n" +
+                    $"Imposto sobre salario: {impostoSalario.ToString("F2")}\n" +
+                    $"Imposto sobre serviços: {impostoServico.ToString("F2")}\n" +
+                    $"Imposto sobre ganho de capital: {impostoCapital.ToString("F2")}\n" +
+                    "\n" +
+                    "DEDUÇÕES:\n" +
+                    $"Máximo dedutível: {maxDedutivel.ToString("F2")}\n" +
+                    $"Gastos dedutíveis: {totalGastos.ToString("F2")}\n" +
+                    "\n" +
+                    "RESUMO:\n" +
+                    $"Imposto bruto total: {brutoImpostos.ToString("F2")}\n" +
+                    $"Abatimento: {totalGastos.ToString("F2")}\n" +
+                    $"Imposto devido: {liquidoImpostos.ToString("F2")}");
+            }
         }
     }
 }
