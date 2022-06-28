d7becb4d58f57a42590522c139f4f329ae250127
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-06-28 15:37:21 -0300

    desafio atletas

--- DevSuperior/DesafioAtletas/DesafioAtletas.cs
+++ DevSuperior/DesafioAtletas/DesafioAtletas.cs
@@ -7,13 +7,16 @@ namespace DesafioAtletas
     {
         static void Main(string[] args)
         {
+            int qtdAtletas = 0;
+            double altura = 0, maiorAltura = 0, peso = 0, pesoTotal = 0, pesoMedioAtletas = 0, alturaTotalMulheres = 0, alturaMediaMulheres = 0, percentagemHomens = 0, qtdHomens = 0, qtdMulheres = 0;
+            //decimal percentagemHomens = 0; (tentativa de corrigir percentagemHomens dando 0, voltei as variaveis pra double e passou oO bug?! why god? why?)
+            //decimal qtdHomens = 0, qtdAtletas = 0, qtdMulheres = 0;
+            string nome = " ", nomeMaisAlto = " ";
+            char sexo = ' ';
+
             Console.WriteLine("Qual a quantidade de atletas? ");
 
-            int qtdAtletas = int.Parse(Console.ReadLine());
-
-            int qtdHomens = 0;
-
-            int qtdMulheres = 0;
+            qtdAtletas = int.Parse(Console.ReadLine());
 
             while (qtdAtletas <= 0)
             {
@@ -24,62 +27,84 @@ namespace DesafioAtletas
 
             int cont = 1;
 
-            double maiorAltura = 0;
-
-            string nomeMaisAlto = " ";
-
             for (i = 1; i <= qtdAtletas; i++)
             {
                 Console.WriteLine($"Informe os dados do atleta número {i}");
                 cont++;
                 Console.WriteLine("Nome: ");
-                string nome = Console.ReadLine();
+                nome = Console.ReadLine();
                 Console.WriteLine("Sexo: ");
-                char sexo = char.Parse(Console.ReadLine());
+                sexo = char.Parse(Console.ReadLine());
 
-                while (sexo != 'M' && sexo != 'm' && sexo != 'F' && sexo != 'f')
+                while (sexo != 'm' && sexo != 'f')
                 {
-                    Console.WriteLine("Sexo invalido, deve ser M,m ou F, f");
+                    Console.WriteLine("Sexo invalido, deve ser m ou f");
+                    sexo = char.Parse(Console.ReadLine());
                 }
 
-                if (sexo == 'M' || sexo == 'm')
+                if (sexo == 'm')
                 {
-                    qtdHomens++;
+
+                    qtdHomens ++;
                 }
                 else
                 {
-                    qtdMulheres++;
+                    qtdMulheres ++;
                 }
 
                 Console.WriteLine("Altura: ");
-                double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 while (altura <= 0)
                 {
                     Console.WriteLine("Altura invalida, insira um valor positivo");
                     altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 }
 
-                maiorAltura = altura;
-                nomeMaisAlto = nome;
-
                 if (altura > maiorAltura)
                 {
                     maiorAltura = altura;
                     nomeMaisAlto = nome;
                 }
-                
+
                 Console.WriteLine("Peso: ");
-                double peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 while (peso <= 0)
                 {
                     Console.WriteLine("Peso invalido, insira um valor positivo");
                     peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 }
 
+                pesoTotal = pesoTotal + peso;
 
+                if (sexo == 'f')
+                {
+                    alturaTotalMulheres = alturaTotalMulheres + altura;
+                }
             }
 
+            pesoMedioAtletas = pesoTotal / qtdAtletas;
 
+            alturaMediaMulheres = alturaTotalMulheres / qtdMulheres;
+
+            percentagemHomens = (qtdHomens / qtdAtletas) * 100.0;
+
+            if(qtdMulheres != 0)
+            {
+                Console.WriteLine("RELATÓRIO FINAL: \n" +
+                $"Peso médio dos atletas: {pesoMedioAtletas.ToString("F2")}\n" +
+                $"Atleta mais alto: {nomeMaisAlto}\n" +
+                $"Porcentagem de homens: {percentagemHomens.ToString("F2")}\n" +
+                $"Altura média das mulheres: {alturaMediaMulheres.ToString("F2")}");
+            }
+
+            else
+            {
+                Console.WriteLine("RELATÓRIO FINAL: \n" +
+                $"Peso médio dos atletas: {pesoMedioAtletas.ToString("F2")}\n" +
+                $"Atleta mais alto: {nomeMaisAlto}\n" +
+                $"Porcentagem de homens: {percentagemHomens.ToString("F2")}\n" +
+                $"Não há mulheres cadastradas");
+            }
         }
     }
 }
