73832528b8c2bbf28d41c6a11109a51b6588d91a
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-07-01 18:02:00 -0300

    desafio concurso

--- DevSuperior/DesafioConcurso/DesafioConcurso.cs
+++ DevSuperior/DesafioConcurso/DesafioConcurso.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
 namespace DesafioConcurso
@@ -7,15 +8,10 @@ namespace DesafioConcurso
     {
         static void Main(string[] args)
         {
-            int qtdPessoas = 0;
-            double percentagemAprovacao = 0.0;
-            var totalParticipantes = 0;
-            var totalAprovados = 0;
+            int qtdPessoas = 0, totalAprovados = 0;
 
             Console.WriteLine("Qual a quantidade de pessoas? ");
             qtdPessoas = int.Parse(Console.ReadLine());
-
-            qtdPessoas += 1; //usei o for para o input, então tenho acrescer uma posição nos vetores que estão dinamicos tbm
 
             string[] nomes = new string[qtdPessoas];
             double[] nota1 = new double[qtdPessoas];
@@ -23,43 +19,47 @@ namespace DesafioConcurso
             double[] mediaCandidatos = new double[qtdPessoas];
             //string[] aprovados = new string[j];
 
-            for (int i = 1; i < qtdPessoas; i++)
+            for (int i = 0; i < qtdPessoas; i++)
             {
                 Console.WriteLine($"Digite os dados da {i}° pessoa: ");
                 Console.WriteLine("Nome: ");
                 nomes[i] = Console.ReadLine();
                 Console.WriteLine("Nota da 1° etapa: ");
-                nota1[i] = double.Parse(Console.ReadLine());
+                nota1[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 Console.WriteLine("Nota da 2° etapa: ");
-                nota2[i] = double.Parse(Console.ReadLine());
+                nota2[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                 mediaCandidatos[i] = (nota1[i] + nota2[i]) / 2;
             }
 
-            for (int j = 1; j < qtdPessoas; j++)
-            {
-                Console.WriteLine("TABELA: \n" +
-                   $"{nomes[j]}, {nota1[j]}, {nota2[j]}, MEDIA = {mediaCandidatos[j]}");
-            }
-
-            for (int n = 1; n < qtdPessoas; n++) { 
-                if (mediaCandidatos[n] > 70.0)
+            Console.WriteLine("\n" +
+                "TABELA:");
+                for (int j = 0; j < qtdPessoas; j++)
                 {
-
-                    Console.WriteLine("PESSOAS APROVADAS: \n" +
-                        $"{nomes[n]}\n");
-
-                    totalAprovados = nomes.Count();
+                    Console.WriteLine($"{nomes[j]}, {nota1[j].ToString("F1")}, {nota2[j].ToString("F1")}, MEDIA = {mediaCandidatos[j].ToString("F2")}");
                 }
 
-            totalParticipantes = nomes.Count();
+            Console.WriteLine("\n" +
+                "APROVADOS:");
 
-            percentagemAprovacao = (totalAprovados / totalParticipantes) * 100;
-                Console.WriteLine($"Percentagem de aprovação: {percentagemAprovacao.ToString("F2"}");
-            
+            for (int n = 0; n < qtdPessoas; n++)
+            {
+                if (mediaCandidatos[n] >= 70)
+                {
+                    Console.WriteLine(nomes[n]);
+                    totalAprovados++;
+                }
             }
 
-            
+
+            int decimalpraPercentagem = 100;
+            decimal percentagemAprovados;
+
+            Console.WriteLine(totalAprovados);
+            percentagemAprovados = (totalAprovados / qtdPessoas) * decimalpraPercentagem;
+
+            Console.WriteLine($"Percentagem de aprovação: {percentagemAprovados.ToString("F2")}");
+          
+            }
         }
     }
-}
