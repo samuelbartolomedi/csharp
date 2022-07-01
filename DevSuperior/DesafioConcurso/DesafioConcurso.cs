523c74ea6f1ea1c3be8d1d38bc9f336fadcba6f4
placebofx94 <samuelbartolomedi1994@gmail.com>
2022-06-30 22:25:29 -0300

    desafio concurso

--- DevSuperior/DesafioConcurso/DesafioConcurso.cs
+++ DevSuperior/DesafioConcurso/DesafioConcurso.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DesafioConcurso
 {
@@ -7,17 +8,20 @@ namespace DesafioConcurso
         static void Main(string[] args)
         {
             int qtdPessoas = 0;
-            double media = 0.0;
-
+            double percentagemAprovacao = 0.0;
+            var totalParticipantes = 0;
+            var totalAprovados = 0;
 
             Console.WriteLine("Qual a quantidade de pessoas? ");
             qtdPessoas = int.Parse(Console.ReadLine());
 
-            qtdPessoas += 1;
+            qtdPessoas += 1; //usei o for para o input, então tenho acrescer uma posição nos vetores que estão dinamicos tbm
 
             string[] nomes = new string[qtdPessoas];
             double[] nota1 = new double[qtdPessoas];
             double[] nota2 = new double[qtdPessoas];
+            double[] mediaCandidatos = new double[qtdPessoas];
+            //string[] aprovados = new string[j];
 
             for (int i = 1; i < qtdPessoas; i++)
             {
@@ -29,10 +33,33 @@ namespace DesafioConcurso
                 Console.WriteLine("Nota da 2° etapa: ");
                 nota2[i] = double.Parse(Console.ReadLine());
 
-                
+                mediaCandidatos[i] = (nota1[i] + nota2[i]) / 2;
             }
 
-            //Console.WriteLine(nomes[i], nota1[i], nota2[i]);
+            for (int j = 1; j < qtdPessoas; j++)
+            {
+                Console.WriteLine("TABELA: \n" +
+                   $"{nomes[j]}, {nota1[j]}, {nota2[j]}, MEDIA = {mediaCandidatos[j]}");
+            }
+
+            for (int n = 1; n < qtdPessoas; n++) { 
+                if (mediaCandidatos[n] > 70.0)
+                {
+
+                    Console.WriteLine("PESSOAS APROVADAS: \n" +
+                        $"{nomes[n]}\n");
+
+                    totalAprovados = nomes.Count();
+                }
+
+            totalParticipantes = nomes.Count();
+
+            percentagemAprovacao = (totalAprovados / totalParticipantes) * 100;
+                Console.WriteLine($"Percentagem de aprovação: {percentagemAprovacao.ToString("F2"}");
+            
+            }
+
+            
         }
     }
 }
