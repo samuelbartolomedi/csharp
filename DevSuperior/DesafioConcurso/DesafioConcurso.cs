7be9c56dea6f2035f986a21c3bfbe798e88397ee
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-07-04 11:07:52 -0300

    desafio concurso

--- DevSuperior/DesafioConcurso/DesafioConcurso.cs
+++ DevSuperior/DesafioConcurso/DesafioConcurso.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Linq;
 
 namespace DesafioConcurso
 {
@@ -8,7 +7,10 @@ namespace DesafioConcurso
     {
         static void Main(string[] args)
         {
-            int qtdPessoas = 0, totalAprovados = 0;
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int qtdPessoas = 0, totalAprovados = 0, contador = 1;
+            double percentagemAprovados = 0.0, maior = 0.0, somaMediaAprovados = 0.0, mediaAprovados = 0.0;
+            string nomeMaior = "";
 
             Console.WriteLine("Qual a quantidade de pessoas? ");
             qtdPessoas = int.Parse(Console.ReadLine());
@@ -17,11 +19,10 @@ namespace DesafioConcurso
             double[] nota1 = new double[qtdPessoas];
             double[] nota2 = new double[qtdPessoas];
             double[] mediaCandidatos = new double[qtdPessoas];
-            //string[] aprovados = new string[j];
 
             for (int i = 0; i < qtdPessoas; i++)
             {
-                Console.WriteLine($"Digite os dados da {i}° pessoa: ");
+                Console.WriteLine($"Digite os dados da {contador}° pessoa: ");
                 Console.WriteLine("Nome: ");
                 nomes[i] = Console.ReadLine();
                 Console.WriteLine("Nota da 1° etapa: ");
@@ -30,36 +31,53 @@ namespace DesafioConcurso
                 nota2[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                 mediaCandidatos[i] = (nota1[i] + nota2[i]) / 2;
+                contador++;
             }
 
             Console.WriteLine("\n" +
                 "TABELA:");
                 for (int j = 0; j < qtdPessoas; j++)
                 {
-                    Console.WriteLine($"{nomes[j]}, {nota1[j].ToString("F1")}, {nota2[j].ToString("F1")}, MEDIA = {mediaCandidatos[j].ToString("F2")}");
+                    Console.WriteLine($"{nomes[j]}, {nota1[j].ToString("F1")}, {nota2[j].ToString("F1")}, MEDIA = {mediaCandidatos[j].ToString("F2", CI)}");
                 }
 
             Console.WriteLine("\n" +
                 "APROVADOS:");
-
             for (int n = 0; n < qtdPessoas; n++)
             {
                 if (mediaCandidatos[n] >= 70)
                 {
-                    Console.WriteLine(nomes[n]);
+                    Console.WriteLine($"{nomes[n]}\n");
+                    somaMediaAprovados = somaMediaAprovados + mediaCandidatos[n];
                     totalAprovados++;
                 }
             }
 
+            percentagemAprovados = ((double)totalAprovados / (double)qtdPessoas) * 100 ;
+            Console.WriteLine($"Percentagem de aprovação: {percentagemAprovados.ToString("F1", CI)} %");
 
-            int decimalpraPercentagem = 100;
-            decimal percentagemAprovados;
+            maior = mediaCandidatos[0];
 
-            Console.WriteLine(totalAprovados);
-            percentagemAprovados = (totalAprovados / qtdPessoas) * decimalpraPercentagem;
+            for (int l = 0; l < qtdPessoas; l++)
+            {
+                if (mediaCandidatos[l] >= maior)
+                {
+                    maior = mediaCandidatos[l];
+                    nomeMaior = nomes[l];
+                }
+            }
 
-            Console.WriteLine($"Percentagem de aprovação: {percentagemAprovados.ToString("F2")}");
-          
+            Console.WriteLine($"Maior média: {nomeMaior}");
+
+            if (totalAprovados <= 0)
+            {
+                Console.WriteLine("Não há candidatos aprovados");
+            }
+            else
+            {
+                mediaAprovados = (double)somaMediaAprovados / (double)totalAprovados;
+                Console.WriteLine($"Nota média dos aprovados: {mediaAprovados.ToString("F2", CI)}");
+            }
             }
         }
     }
