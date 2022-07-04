b6a7f142b52aaffb5f430fe8288b8993015f8df7
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-07-04 18:04:50 -0300

    desafio telefone

---
+++ DevSuperior/DesafioTelefone/DesafioTelefone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DesafioTelefone
+{
+    internal class DesafioTelefone
+    {
+        static void Main(string[] args)
+        {
+            int qtdClientes = 0, contador = 1;
+
+            string[] nomes = new string[qtdClientes];
+            string[] telefones = new string[qtdClientes];
+            int[] tipos = new int[qtdClientes];
+            double[] minutos = new double[qtdClientes];
+
+            Console.WriteLine("Informe a quantidade de clientes: ");
+            qtdClientes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdClientes; i++)
+            {
+                Console.WriteLine($"Dados do {contador}° cliente:");
+                Console.WriteLine("Nome? ");
+                nomes[i] = Console.ReadLine();
+                Console.WriteLine("Telefone? ");
+                telefones[i] = Console.ReadLine();
+                Console.WriteLine("Tipo? ");
+                tipos[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Minutos? ");
+                minutos[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Informe o preço básico e excedente de cada tipo de conta: ");
+
+            for (int j = 0; j < 2; j++)
+            {
+                Console.WriteLine($"Tipo {j}? ");
+            }
+        }
+    }
+}
