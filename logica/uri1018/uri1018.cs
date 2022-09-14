a38b0d9749d3463daa97fd463ddee9a7506dc839
placebofx94 <samuelbartolomedi1994@gmail.com>
2022-09-13 23:35:39 -0300

    AspNetCore project

---
+++ logica/uri1018/uri1018.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace uri1018
+{
+    internal class uri1018
+    {
+        static void Main(string[] args)
+        {
+            int N, cedulas, resto;
+
+            N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(N);
+
+            cedulas = N / 100;
+            resto = N % 100;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 100,00");
+
+            cedulas = resto / 50;
+            resto = resto % 50;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 50,00");
+            
+            cedulas = resto / 20;
+            resto = resto % 20;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 20,00");
+
+            cedulas = resto / 10;
+            resto = resto % 10;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 10,00");
+
+            cedulas = resto / 5;
+            resto = resto % 5;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 5,00");
+
+            cedulas = resto / 2;
+            resto = resto % 2;
+
+            Console.WriteLine($"{cedulas} nota(s) de R$ 2,00");
+
+            cedulas = resto / 1; //nem precisaria? pq?
+            Console.WriteLine($"{cedulas} nota(s) de R$ 1,00");
+        }
+    }
+}
---
+++ logica/uri1047/uri1047.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace uri1047
+{
+    internal class uri1047
+    {
+        static void Main(string[] args)
+        {
+            string[] horarios = Console.ReadLine().Split(' ');
+            int hr_inicio = int.Parse(horarios[0]);
+            int min_inicio = int.Parse(horarios[1]);
+            int hr_fim = int.Parse(horarios[2]);
+            int min_fim = int.Parse(horarios[3]);
+
+            int instante_inicial = hr_inicio * 60 + min_inicio; // transformou em minutos?
+            int instante_final = hr_fim * 60 + min_fim;         // transformou em minutos?
+
+            int duracao;
+
+            if (instante_inicial < instante_final)
+            {
+                duracao = instante_final - instante_inicial;
+            }
+            else
+            {
+                duracao = (24 * 60 - instante_inicial) + instante_final;
+            }
+
+            int duracao_hr = duracao / 60;  //divide o total de minutos calculado dentro do if (poderia ser uma função)
+            int duracao_min = duracao % 60; //divide o total de minutos calculado dentro do if (poderia ser uma função)
+
+            Console.WriteLine("O JOGO DUROU " + duracao_hr + " HORA(S) E " + duracao_min + " MINUTO(S)");
+        }
+    }
+}
