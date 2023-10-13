0d6014b50691b18aaddad284fac50cf4b12d725c
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2023-10-12 23:03:43 -0300

    alterações

---
+++ crudFineRazor/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using crudFineRazor.Data;
+using crudFineRazor.Models;
+
+namespace crudFineRazor.Pages_Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Students == null || Student == null)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
